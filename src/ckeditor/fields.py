7b0dd53eb19933450d05106cf161dd22789e89a2
David Charbonnier <d.charbonnier@oxys.net>
2013-08-21 10:19:43 +0200

    manual integration of theatlantic fork

--- src/ckeditor/fields.py
+++ src/ckeditor/fields.py
@@ -2,29 +2,58 @@ from django.db import models
 from django import forms
 
 from ckeditor.widgets import CKEditorWidget
+from ckeditor.utils.image_resize import resize_images
 
 
-class RichTextField(models.TextField):
+class RichModelFieldMixin(object):
+
     def __init__(self, *args, **kwargs):
         self.config_name = kwargs.pop("config_name", "default")
-        super(RichTextField, self).__init__(*args, **kwargs)
+        self.dynamic_resize = kwargs.pop("dynamic_resize", False)
+        self.extra_config = kwargs.pop('extra_config', {})
+        super(RichModelFieldMixin, self).__init__(*args, **kwargs)
 
     def formfield(self, **kwargs):
         defaults = {
             'form_class': RichTextFormField,
             'config_name': self.config_name,
+            'dynamic_resize': self.dynamic_resize,
+            'extra_config': self.extra_config,
         }
-        defaults.update(kwargs)
-        return super(RichTextField, self).formfield(**defaults)
+        defaults.update(kwargs) # Adds request if it's there too
+        return super(RichModelFieldMixin, self).formfield(**defaults)
 
 
-class RichTextFormField(forms.fields.Field):
+class RichCharField(RichModelFieldMixin, models.CharField):
+    pass
+
+
+class RichTextField(RichModelFieldMixin, models.TextField):
+    pass
+
+
+class RichTextFormField(forms.CharField):
+
     def __init__(self, config_name='default', *args, **kwargs):
-        kwargs.update({'widget': CKEditorWidget(config_name=config_name)})
+        self.dynamic_resize = kwargs.pop("dynamic_resize", False)
+        self.extra_config = kwargs.pop('extra_config', {})
+        kwargs.update({
+            'widget': CKEditorWidget(config_name=config_name, config=self.extra_config),
+        })
+        self.request = kwargs.pop('request', None)
         super(RichTextFormField, self).__init__(*args, **kwargs)
+
+    def to_python(self, value):
+        value = super(RichTextFormField, self).to_python(value)
+        if self.dynamic_resize:
+            value = resize_images(value, request=self.request)
+        return value
+
 
 try:
     from south.modelsinspector import add_introspection_rules
-    add_introspection_rules([], ["^ckeditor\.fields\.RichTextField"])
-except:
+except ImportError:
     pass
+else:
+    add_introspection_rules([], ["^ckeditor\.fields\.RichCharField"])
+    add_introspection_rules([], ["^ckeditor\.fields\.RichTextField"])
--- src/ckeditor/widgets.py
+++ src/ckeditor/widgets.py
@@ -1,34 +1,12 @@
 from django import forms
-from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.template.loader import render_to_string
 from django.utils.safestring import mark_safe
-from django.utils.html import conditional_escape
 from django.utils.encoding import force_unicode
-from django.core.exceptions import ImproperlyConfigured
+from django.utils.html import conditional_escape
+
 from django.forms.util import flatatt
-import json
 
-
-json_encode = json.JSONEncoder().encode
-
-DEFAULT_CONFIG = {
-    'skin': 'moono',
-    'toolbar_Basic': [
-        ['Source', '-', 'Bold', 'Italic']
-    ],
-    'toolbar_Full': [
-        ['Styles', 'Format', 'Bold', 'Italic', 'Underline', 'Strike', 'SpellChecker', 'Undo', 'Redo'],
-        ['Image', 'Flash', 'Table', 'HorizontalRule'],
-        ['TextColor', 'BGColor'],
-        ['Smiley', 'SpecialChar'], ['Source'],
-    ],
-    'toolbar': 'Full',
-    'height': 291,
-    'width': 835,
-    'filebrowserWindowWidth': 940,
-    'filebrowserWindowHeight': 725,
-}
+from . import settings as ck_settings, utils
 
 
 class CKEditorWidget(forms.Textarea):
@@ -36,54 +14,53 @@ class CKEditorWidget(forms.Textarea):
     Widget providing CKEditor for Rich Text Editing.
     Supports direct image uploads and embed.
     """
-    class Media:
-        try:
-            js = (
-                settings.STATIC_URL + 'ckeditor/ckeditor/ckeditor.js',
-            )
-        except AttributeError:
-            raise ImproperlyConfigured("django-ckeditor requires \
-                    CKEDITOR_MEDIA_PREFIX setting. This setting specifies a \
-                    URL prefix to the ckeditor JS and CSS media (not \
-                    uploaded media). Make sure to use a trailing slash: \
-                    CKEDITOR_MEDIA_PREFIX = '/media/ckeditor/'")
 
-    def __init__(self, config_name='default', *args, **kwargs):
-        super(CKEditorWidget, self).__init__(*args, **kwargs)
+    #: space-separated list of css classes applied to the ckeditor textarea
+    ckeditor_classes = ("django-ckeditor-textarea",)
+
+    def __init__(self, config_name=None, config=None, attrs=None):
+        super(CKEditorWidget, self).__init__(attrs)
         # Setup config from defaults.
-        self.config = DEFAULT_CONFIG.copy()
+        self.config_name = config_name
+        self.extra_config = config
+        self.config = utils.validate_config(config_name=config_name, config=config)
 
-        # Try to get valid config from settings.
-        configs = getattr(settings, 'CKEDITOR_CONFIGS', None)
-        if configs:
-            if isinstance(configs, dict):
-                # Make sure the config_name exists.
-                if config_name in configs:
-                    config = configs[config_name]
-                    # Make sure the configuration is a dictionary.
-                    if not isinstance(config, dict):
-                        raise ImproperlyConfigured('CKEDITOR_CONFIGS["%s"] \
-                                setting must be a dictionary type.' % \
-                                config_name)
-                    # Override defaults with settings config.
-                    self.config.update(config)
-                else:
-                    raise ImproperlyConfigured("No configuration named '%s' \
-                            found in your CKEDITOR_CONFIGS setting." % \
-                            config_name)
-            else:
-                raise ImproperlyConfigured('CKEDITOR_CONFIGS setting must be a\
-                        dictionary type.')
+    @property
+    def media(self):
+        media_prefix = ck_settings.MEDIA_PREFIX
+        if len(media_prefix) and media_prefix[-1] != '/':
+            media_prefix += '/'
+        source_dir = '%sckeditor/ckeditor' % media_prefix
 
-    def render(self, name, value, attrs={}):
+        timestamp = ck_settings.TIMESTAMP
+
+        if ck_settings.CKEDITOR_DEBUG:
+            source_dir = "%s-dev" % source_dir
+
+        media = super(CKEditorWidget, self).media
+        media.add_js([
+            '%s/ckeditor.js?timestamp=%s' % (source_dir, timestamp),
+            reverse('ckeditor_configs'),
+            '%s/core/adapters/jquery.js?timestamp=%s' % (source_dir, timestamp),
+            '%s/ckeditor_widget.js?timestamp=%s' % (source_dir, timestamp),
+        ])
+        return media
+
+    def render(self, name, value, attrs=None):
         if value is None:
             value = ''
-        final_attrs = self.build_attrs(attrs, name=name)
-        self.config['filebrowserUploadUrl'] = reverse('ckeditor_upload')
-        self.config['filebrowserBrowseUrl'] = reverse('ckeditor_browse')
-        return mark_safe(render_to_string('ckeditor/widget.html', {
-            'final_attrs': flatatt(final_attrs),
+        value = utils.swap_in_originals(value)
+
+        attrs = attrs or {}
+
+        classes = attrs.pop('class', [])
+        attrs['class'] = utils.combine_css_classes(classes, self.ckeditor_classes)
+        if self.config_name:
+            attrs['data-config-name'] = self.config_name
+        if self.extra_config:
+            attrs['data-config'] = utils.json_encode(self.extra_config)
+
+        return mark_safe(u'<textarea%(attrs)s>%(value)s</textarea>' % {
+            'attrs': flatatt(self.build_attrs(attrs, name=name)),
             'value': conditional_escape(force_unicode(value)),
-            'id': final_attrs['id'],
-            'config': json_encode(self.config)
-        }))
+        })
